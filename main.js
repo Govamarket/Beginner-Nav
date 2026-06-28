7a53d0a2cf6a9fa7cdcf1e8aaf57e5c818019c56
Govamarket <govaway@gmail.com>
2026-06-28 16:37:15 +0100

    initial commit

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
-  console.log("HELLO WORLD");
   navLinks.classList.toggle("active");
 });
